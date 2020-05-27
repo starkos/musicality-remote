7325197bed1d4b149d52b2b78737c524bdfaf183
starkos <starkos@industriousone.com>
2020-05-27 16:09:21 -0400

    Bootstrap Forms projects

---
+++ Musicality.Remote.iOS/AppDelegate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Musicality.Remote.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new Musicality.Remote.Forms.App());
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ Musicality.Remote.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Musicality.Remote.iOS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Industrious One")]
+[assembly: AssemblyProduct("Musicality.Remote")]
+[assembly: AssemblyCopyright("Copyright © Industrious One 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("72bdc44f-c588-44f3-b6df-9aace7daafdd")]
